186c6490a7be2de7300bb0b5986efcfd15f9541a
nikhil1905-n <nikhil1905.n@gmail.com>
2024-06-05 08:41:44 +0530

    Implement util layer

--- src/main/java/com/nikhiln/rentRead/util/PasswordEncoderUtil.java
+++ src/main/java/com/nikhiln/rentRead/util/PasswordEncoderUtil.java
@@ -1,5 +1,16 @@
 package com.nikhiln.rentRead.util;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class PasswordEncoderUtil {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     
 }
